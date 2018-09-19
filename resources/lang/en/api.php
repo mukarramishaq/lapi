a51e39a2f0ad12424ffc334fbde292821812fba0
Mukarram Ishaq <mukarram.ishaq@rolustech.net>
2018-09-19 18:23:22 +0500

    create localization files

---
+++ resources/lang/en/api.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Api Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during api for various
+    | messages that we need to display to the user.
+    |
+    */
+
+    'auth' => [
+        'failed' => [
+            "login" => ["message"=>'These credentials do not match our records.']
+        ],
+        'success' => [
+            "logout" => ["message" => 'You have been logged out successfully'],
+        ],
+    ],
+
+];
